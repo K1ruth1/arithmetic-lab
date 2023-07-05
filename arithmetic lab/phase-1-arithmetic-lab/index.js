b92ce2faf39ab84c5aa97fc6b36c247cf8600c78
K1ruth1 <135802282+K1ruth1@users.noreply.github.com>
2023-07-05 11:24:41 +0300

    Add files via upload

---
+++ arithmetic lab/phase-1-arithmetic-lab/index.js
@@ -0,0 +1,14 @@
+
+// Write your code here
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2; // multiply = 62
+
+let random = Math.floor(Math.random() * 100) + 1; // random integer between 1 and 100
+
+let num3 = 25;
+let num4 = 7;
+let mod = num3 % num4; // mod = 4
+
+let numbers = [5, 10, 15, 20];
+let max = Math.max(...numbers); // max = 20
